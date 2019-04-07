1fc0414c46bcb43357bf8e26df16f0ee974688cf
PoopFace98 <32722225+PoopFace98@users.noreply.github.com>
2019-04-07 01:56:10 +0100

    Update Projeto.java

--- ProjetoGrupo/src/Backend/Projeto.java
+++ ProjetoGrupo/src/Backend/Projeto.java
@@ -4,28 +4,71 @@ import java.time.LocalDate;
 
 public class Projeto {
 
-    private String Titulo;
-    int Descrição;
-    LocalDate DataInicio;
-    LocalDate DataFim;
-    ListaTarefasProjeto Tarefas;
-    Gestor Gestor;
-    Estado EstadoProjeto;
+    private String titulo;
+    int descrição;
+    LocalDate dataInicio;
+    LocalDate dataFim;
+    ListaTarefasProjeto tarefas;
+    Gestor gestor;
+    Estado estadoProjeto;
 
     public String getTitulo() {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
+        return titulo;
     }
 
-    public String getDescricao() {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
-    public void setTitulo(String Titulo) {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
+    public int getDescrição() {
+        return descrição;
     }
 
-    public void setDescrição(String Descrição) {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
+    public void setDescrição(int descrição) {
+        this.descrição = descrição;
     }
 
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public ListaTarefasProjeto getTarefas() {
+        return tarefas;
+    }
+
+    public void setTarefas(ListaTarefasProjeto tarefas) {
+        this.tarefas = tarefas;
+    }
+
+    public Gestor getGestor() {
+        return gestor;
+    }
+
+    public void setGestor(Gestor gestor) {
+        this.gestor = gestor;
+    }
+
+    public Estado getEstadoProjeto() {
+        return estadoProjeto;
+    }
+
+    public void setEstadoProjeto(Estado estadoProjeto) {
+        this.estadoProjeto = estadoProjeto;
+    }
+
+    
+
+    
 }
