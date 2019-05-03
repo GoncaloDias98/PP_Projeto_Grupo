e8e823b606bab067564254c72d2c7fff96ca9b61
jacintobessa <50250523+jacintobessa@users.noreply.github.com>
2019-05-03 22:00:46 +0100

    Revert "Update Colaborador.java"
    This reverts commit c9bed171fcdb5df0316db5cc882e2eaf8d446d86, reversing
    changes made to 7227d74aaeb27515ccc7735337f5604b8e28e39f.

--- src/BackEnd/Colaborador.java
+++ src/BackEnd/Colaborador.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package BackEnd;
 
 //vai pro PUTA QUE TE PARIU
@@ -16,22 +15,3 @@ public class Colaborador extends Utilizador {
     }
     
 }
-=======
-package BackEnd;
-
-
-public class Colaborador extends Utilizador {
-
-    public Colaborador() {
-    }
-
-    public Colaborador(String user, String password, String nome, String morada, String telefone, String email) {
-        super(user, password, nome, morada, telefone, email);
-    }
- public void ConsultaColaborador() {
-        System.out.println("username: " + super.getNome() + ", password: " + super.getPassword() + ", nome: " +   super.getNome() + ", morada: " + super.getMorada() + ", telefone: " + super.getTelefone() + ", email: " + super.getEmail() );
-    
-    }
-    
-}
->>>>>>> 5df3877a411513de2609635e95324e7b100ab382
